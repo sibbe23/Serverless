93440999fd2e8505ce963de77fe413398bf5913d
sibbe23 <sibbe23@gmail.com>
2024-10-16 11:13:34 +0530

    final commit 1

--- models/vendor.js
+++ models/vendor.js
@@ -13,16 +13,7 @@ const sequelize = new Sequelize(
   }
 );
 
-const testConnection = async () => {
-    try {
-        await sequelize.authenticate();
-        console.log('Connection to the database has been established successfully.');
-    } catch (error) {
-        console.error('Unable to connect to the database:', error);
-    }
-};
 
-testConnection();
 
 const Vendor = sequelize.define('Vendor', {
   name: {
